a35e7a7bf84ff09bea99d2b71b19b9785c8b8481
chrisvidal <chris@voilaah.com>
2019-05-05 13:16:42 +0800

    content updates

--- source/resources.blade.php
+++ source/resources.blade.php
@@ -6,7 +6,7 @@
     <div class="section__content">
         <div class="g">
             <div class="g-item g-item--lg">
-                <h1 class="h1">You will find books, articles, videos, friends, men's groups,...</h1>
+                <h1 class="h1">Books, articles, videos, friends, men's groups, quotes...</h1>
             </div>
         </div>
     </div>
@@ -21,32 +21,44 @@
             <div class="g-item g-item--lg">
                 <div class="title">Books</div>
             </div>
+            <div class="g-item g-item--lg push-top--x-large">
+                <div class="g-sub">
+                    <div class="book">
+                        <div class="h2 tr-basic tr-basic--enter"><a target="_blank" href="https://www.amazon.com/Way-Superior-Man-Challenges-Anniversary-ebook/dp/B01NA993PI/" class="a a--subtle">The Way of The Superior Man</a></div>
+                        <div class="description push-top--large tr-basic tr-basic--enter">by David Deida</div>
+                        <div class="image-content__copy push-top--large tr-basic tr-basic--enter">
+                            <p><img alt="The Way of the Superior Man: A Spiritual Guide to Mastering the Challenges of Women, Work, and Sexual Desire (20th Anniversary Edition) by [Deida, David]" src="https://images-na.ssl-images-amazon.com/images/I/61iwy8p4gjL.jpg" style="position: relative; float: left; top: 0px; left: 0px; width: 20%; height: auto; margin-right: .7em;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A soluta minima voluptatum fugit necessitatibus, recusandae aut blanditiis qui ipsam fuga amet quidem asperiores aspernatur iure, accusantium unde in reprehenderit. Excepturi.</p>
+                        </div>
+                    </div>
+                    <div class="book">
+                        <div class="h2 tr-basic tr-basic--enter push-top--large"><a target="_blank" href="https://www.amazon.com/Be-Man-Guide-Masculine-Power-ebook/dp/B00R54N966/" class="a a--subtle">To Be a Man: A Guide to True Masculine Power</a></div>
+                        <div class="description push-top--large tr-basic tr-basic--enter">by Robert A. Masters</div>
+                        <div class="image-content__copy push-top--large tr-basic tr-basic--enter">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A soluta minima voluptatum fugit necessitatibus, recusandae aut blanditiis qui ipsam fuga amet quidem asperiores aspernatur iure, accusantium unde in reprehenderit. Excepturi.</p>
+                        </div>
+                    </div>
+                    <div class="book">
+                        <div class="h2 tr-basic tr-basic--enter"><a target="_blank" href="https://www.amazon.com/Trauma-Everyday-Life-Mark-Epstein/dp/0143125745" class="a a--subtle">The Trauma of Everyday Life</a></div>
+                        <div class="description push-top--large tr-basic tr-basic--enter">by Mark Epstein M.D.</div>
+                        <div class="image-content__copy push-top--large tr-basic tr-basic--enter">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A soluta minima voluptatum fugit necessitatibus, recusandae aut blanditiis qui ipsam fuga amet quidem asperiores aspernatur iure, accusantium unde in reprehenderit. Excepturi.</p>
+                        </div>
+                    </div>
+                    <div class="book">
+                        <div class="h2 tr-basic tr-basic--enter push-top--large">Book 4</div>
+                        <div class="description push-top--large tr-basic tr-basic--enter"><a target="_blank" href="#" class="a a--subtle">Book 4 url</a></div>
+                        <div class="image-content__copy push-top--large tr-basic tr-basic--enter">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A soluta minima voluptatum fugit necessitatibus, recusandae aut blanditiis qui ipsam fuga amet quidem asperiores aspernatur iure, accusantium unde in reprehenderit. Excepturi.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="g-item push-top--large">
                 <div class="content__column tr-basic tr-basic--enter">
-                    <div class="h2 tr-basic tr-basic--enter">Book 1</div>
-                    <div class="description push-top--large tr-basic tr-basic--enter"><a href="#" class="a a--subtle">Book 1 url</a></div>
-                    <div class="image-content__copy push-top--large tr-basic tr-basic--enter">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A soluta minima voluptatum fugit necessitatibus, recusandae aut blanditiis qui ipsam fuga amet quidem asperiores aspernatur iure, accusantium unde in reprehenderit. Excepturi.</p>
-                    </div>
-                    <div class="h2 tr-basic tr-basic--enter push-top--large">Book 3</div>
-                    <div class="description push-top--large tr-basic tr-basic--enter"><a href="#" class="a a--subtle">Book 3 url</a></div>
-                    <div class="image-content__copy push-top--large tr-basic tr-basic--enter">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A soluta minima voluptatum fugit necessitatibus, recusandae aut blanditiis qui ipsam fuga amet quidem asperiores aspernatur iure, accusantium unde in reprehenderit. Excepturi.</p>
-                    </div>
                 </div>
             </div>
             <div class="g-item g-item--col-2 push-top--large">
                 <div class="content__column tr-basic tr-basic--enter">
-                    <div class="h2 tr-basic tr-basic--enter">Book 2</div>
-                    <div class="description push-top--large tr-basic tr-basic--enter"><a href="#" class="a a--subtle">Book 2 url</a></div>
-                    <div class="image-content__copy push-top--large tr-basic tr-basic--enter">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A soluta minima voluptatum fugit necessitatibus, recusandae aut blanditiis qui ipsam fuga amet quidem asperiores aspernatur iure, accusantium unde in reprehenderit. Excepturi.</p>
-                    </div>
-                    <div class="h2 tr-basic tr-basic--enter push-top--large">Book 4</div>
-                    <div class="description push-top--large tr-basic tr-basic--enter"><a href="#" class="a a--subtle">Book 4 url</a></div>
-                    <div class="image-content__copy push-top--large tr-basic tr-basic--enter">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A soluta minima voluptatum fugit necessitatibus, recusandae aut blanditiis qui ipsam fuga amet quidem asperiores aspernatur iure, accusantium unde in reprehenderit. Excepturi.</p>
-                    </div>
                 </div>
             </div>
         </div>
